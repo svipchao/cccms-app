dd5ccaa04e4ae3fb580a4dbb236b5be1ad155583
诗无尽头i <svipchao@qq.com>
2022-05-30 18:08:52 +0800

    完善类别管理

--- src/admin/model/SysTypes.php
+++ src/admin/model/SysTypes.php
@@ -5,6 +5,7 @@ namespace app\admin\model;
 
 use think\facade\Cache;
 use cccms\Model;
+use think\model\relation\HasMany;
 
 class SysTypes extends Model
 {
@@ -13,6 +14,39 @@ class SysTypes extends Model
     public static function onBeforeWrite($model)
     {
         Cache::delete('SysTypes');
+    }
+
+    // 删除前
+    public static function onBeforeDelete($model)
+    {
+        $data = $model->withCount(['menus', 'configs', 'routes', 'files'])->_read($model['id'], function ($data) {
+            $data = $data->toArray();
+            $data['data_count'] = $data['menus_count'] + $data['configs_count'] + $data['routes_count'] + $data['files_count'];
+            return $data;
+        });
+        if ($data['data_count'] > 0) {
+            _result(['code' => 403, 'msg' => '类别下存在数据，禁止删除'], _getEnCode());
+        }
+    }
+
+    public function menus(): HasMany
+    {
+        return $this->hasMany('SysMenu', 'type_id', 'id');
+    }
+
+    public function configs(): HasMany
+    {
+        return $this->hasMany('SysConfig', 'type_id', 'id');
+    }
+
+    public function routes(): HasMany
+    {
+        return $this->hasMany('SysRoute', 'type_id', 'id');
+    }
+
+    public function files(): HasMany
+    {
+        return $this->hasMany('SysFile', 'type_id', 'id');
     }
 
     public function getTypeTextAttr($value, $data): string
