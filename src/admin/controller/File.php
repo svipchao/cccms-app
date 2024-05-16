29dd1ad6b83062c104031699d0ff502a378792d4
诗无尽头i <svipchao@qq.com>
2024-05-16 09:18:05 +0800

    init .

---
+++ src/admin/controller/File.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types=1);
+
+namespace app\admin\controller;
+
+use cccms\{Base, Storage};
+use cccms\model\{SysFile, SysFileCate};
+use cccms\services\{AuthService, UploadService};
+
+/**
+ * 附件管理
+ * @sort 991
+ */
+class File extends Base
+{
+    public function init()
+    {
+        $this->model = SysFile::mk();
+    }
+
+    /**
+     * 添加附件
+     * @auth true
+     * @login true
+     * @encode json|jsonp|xml
+     * @methods POST
+     */
+    public function create()
+    {
+        $cate_id = $this->request->post('cate_id/d', 0);
+        _result([
+            'code' => 200,
+            'msg' => '添加成功',
+            'data' => UploadService::instance()->upload($cate_id)
+        ], _getEnCode());
+    }
+
+    /**
+     * 删除附件
+     * @auth true
+     * @login true
+     * @encode json|jsonp|xml
+     * @methods DELETE
+     */
+    public function delete()
+    {
+        Storage::instance()->delete($this->request->delete('id/d', 0));
+        _result(['code' => 200, 'msg' => '删除成功'], _getEnCode());
+    }
+
+    /**
+     * 修改附件
+     * @auth true
+     * @login true
+     * @encode json|jsonp|xml
+     * @methods PUT
+     */
+    public function update()
+    {
+        $params = _validate('put.sys_file.false', 'id|file_name,file_desc,extract_code,status');
+        $this->model->update($params);
+        _result(['code' => 200, 'msg' => '更新成功'], _getEnCode());
+    }
+
+    /**
+     * 附件列表
+     * @auth true
+     * @login true
+     * @encode json|jsonp|xml
+     * @methods GET
+     */
+    public function index()
+    {
+        $cates = SysFileCate::mk()->_list();
+        $cate_id = $this->request->get('cate_id/d', null);
+        $cate_id = $cate_id ?: ($cates[0]['id'] ?? 0);
+        $params = _validate('get.sys_file.true', 'page,limit|cate_id');
+        $data = $this->model->with(['cate', 'user'])->_withSearch('cate_id', [
+            'cate_id' => $params['cate_id'],
+        ])->order('id desc')->_page($params);
+        _result(['code' => 200, 'msg' => 'success', 'data' => [
+            'fields' => AuthService::instance()->fields('sys_file'),
+            'cates' => $cates,
+            'total' => $data['total'],
+            'data' => $data['data']
+        ]], _getEnCode());
+    }
+}
---
+++ src/admin/controller/User.php
@@ -0,0 +1,108 @@
+<?php
+declare(strict_types=1);
+
+namespace app\admin\controller;
+
+use cccms\Base;
+use cccms\model\SysDept;
+use cccms\model\SysRole;
+use cccms\model\SysUser;
+use cccms\services\{UserService, AuthService};
+
+/**
+ * 用户管理
+ * @sort 995
+ */
+class User extends Base
+{
+    public function init(): void
+    {
+        $this->model = SysUser::mk();
+    }
+
+    /**
+     * 添加用户
+     * @auth true
+     * @login true
+     * @encode json|jsonp|xml
+     * @methods POST
+     */
+    public function create(): void
+    {
+        $params = _validate('post.sys_user.true', 'nickname,username,password|role_ids,dept_ids');
+        $params['password'] = md5($params['password']);
+        $this->model->save($params);
+        _result(['code' => 200, 'msg' => '添加成功'], _getEnCode());
+    }
+
+    /**
+     * 删除用户
+     * @auth true
+     * @login true
+     * @encode json|jsonp|xml
+     * @methods DELETE
+     */
+    public function delete(): void
+    {
+        $this->model->_delete($this->request->delete('id/d', 0));
+        _result(['code' => 200, 'msg' => '删除成功'], _getEnCode());
+    }
+
+    /**
+     * 更新用户
+     * @auth true
+     * @login true
+     * @encode json|jsonp|xml
+     * @methods PUT
+     */
+    public function update(): void
+    {
+        $params = _validate('put.sys_user.true', 'id|role_ids,dept_ids');
+        if (!empty($params['password'])) {
+            $params['password'] = md5($params['password']);
+        } else {
+            unset($params['password']);
+        }
+        $user = $this->model->where('id', $params['id'])->findOrEmpty();
+        if ($user->isEmpty()) {
+            _result(['code' => 403, 'msg' => '用户不存在'], _getEnCode());
+        }
+        $user->save($params);
+        _result(['code' => 200, 'msg' => '更新成功'], _getEnCode());
+    }
+
+    /**
+     * 用户列表
+     * @auth true
+     * @login true
+     * @encode json|jsonp|xml
+     * @methods GET
+     */
+    public function index(): void
+    {
+        $params = _validate('get.sys_user.true', 'page,limit|user,tag,type,dept_id');
+        $users = $this->model->_withSearch('user,tag,dept_id', [
+            'user' => $params['user'] ?? null,
+            'tag' => $params['tag'] ?? null,
+            'type' => $params['type'] ?? null,
+            'dept_id' => $params['dept_id'] ?? null,
+        ])->with(['depts', 'roles'])->_page($params, false, function ($data) {
+            $data = $data->toArray();
+            $data['data'] = array_map(function ($item) {
+                $item['dept_ids'] = array_column($item['depts'], 'id');
+                $item['depts'] = array_column($item['depts'], 'dept_name');
+                $item['role_ids'] = array_column($item['roles'], 'id');
+                $item['roles'] = array_column($item['roles'], 'role_name');
+                return $item;
+            }, $data['data']);
+            return $data;
+        });
+        _result(['code' => 200, 'msg' => 'success', 'data' => [
+            'fields' => AuthService::instance()->fields('sys_user'),
+            'roles' => SysRole::mk()->getAllOpenRole(true),
+            'depts' => UserService::instance()->getUserDepts('tree'),
+            'total' => $users['total'] ?? 0,
+            'data' => $users['data'] ?? [],
+        ]], _getEnCode());
+    }
+}
